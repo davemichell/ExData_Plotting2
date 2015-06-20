123ac401251c129163119445ada77d40156d6cdf
David Michell <davemichell@gmail.com>
2015-06-20 17:01:18 +1000

    Completion of Plot 2
    png and R script

--- plot2.R
+++ plot2.R
@@ -5,8 +5,10 @@ plot2 <- function(){
     PM_data <- readRDS("./data/summarySCC_PM25.rds")
     ## codeBook <- readRDS("./data/Source_Classification_Code.rds")
     
+    Balt_City_data <- PM_data[which(PM_data$fips=="24510"), ]
+    rm(PM_data)
     ## aggregate data by year
-    PMagg <- aggregate(PM_data$Emissions, list(PM_data$year), sum)
+    PMagg <- aggregate(Balt_City_data$Emissions, list(Balt_City_data$year), sum)
     names(PMagg)[names(PMagg)=="Group.1"] <- "Year"
     names(PMagg)[names(PMagg)=="x"] <- "Emissions"
     
@@ -15,7 +17,7 @@ plot2 <- function(){
     
     ## plot the data!
     plot(PMagg$Year, PMagg$Emissions, type="b", xlab="Year", ylab="Total Emissions",
-         main="Total US Emissions")
+         main="Baltimore City Emissions")
 
     ## if the plot is sent to the screen, uncomment below to copy to a file
     ## dev.copy(png, file= "plot2.png") 
