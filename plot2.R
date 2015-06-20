8c077ed1638a3af9beee03213981a09e843c0719
David Michell <davemichell@gmail.com>
2015-06-20 16:52:04 +1000

    Completion of Plot 1 and start of plot 2
    Completed plot1.png

--- plot2.R
+++ plot2.R
@@ -1,4 +1,4 @@
-plot1 <- function(){
+plot2 <- function(){
     
     ## Load data
     ## assumes data is in data directory in working directory
@@ -11,15 +11,15 @@ plot1 <- function(){
     names(PMagg)[names(PMagg)=="x"] <- "Emissions"
     
     ## if line below is commented out, the plot will be sent to the screen
-    png(file="plot1.png",width=480,height=480) 
+    png(file="plot2.png",width=480,height=480) 
     
     ## plot the data!
     plot(PMagg$Year, PMagg$Emissions, type="b", xlab="Year", ylab="Total Emissions",
          main="Total US Emissions")
 
     ## if the plot is sent to the screen, uncomment below to copy to a file
-    ## dev.copy(png, file= "plot1.png") 
+    ## dev.copy(png, file= "plot2.png") 
     dev.off()
     
-    print("Plot saved as plot1.png in working directory")
+    print("Plot saved as plot2.png in working directory")
 }
