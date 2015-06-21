632ab77ea7c6851836e8655da35448027deba46e
David Michell <davemichell@gmail.com>
2015-06-21 14:29:40 +1000

    Final script with comments

--- plot6.R
+++ plot6.R
@@ -14,7 +14,7 @@ plot6 <- function(){
     road <- grep("Onroad",codeBook$Data.Category)
     CB_road <- codeBook[road, ]
     
-    ## Subset from subsetted codebook 
+    ## Subset PM data from subsetted codebook 
     PM_road <- filter(PM_data, SCC %in% CB_road$SCC)
     
     ## Select only the data for Baltimore City & LA County
@@ -23,17 +23,22 @@ plot6 <- function(){
     rm(PM_data) ## remove the full data set
     rm(codeBook) ## remove full codebook
     
-    ## aggregate data by year
+    ## aggregate data by year for each location
     PMagg_BC <- summarise(group_by(BC_road_data, Year=year), 
                        Emissions=sum(Emissions))
-    PMagg_BC$Location = "Baltimore City"
-    PMagg_BC$Comp  <- PMagg_BC$Emissions/ (PMagg_BC[which(PMagg_BC$Year==1999),]$Emissions)
+    PMagg_BC$Location = "Baltimore City" ## add location variable
+    PMagg_BC$Comp  <- PMagg_BC$Emissions/ 
+        (PMagg_BC[which(PMagg_BC$Year==1999),]$Emissions) ## add normalised comparison
     
     PMagg_LA <- summarise(group_by(LA_road_data, Year=year), 
                           Emissions=sum(Emissions))
-    PMagg_LA$Location = "Los Angeles County"
-    PMagg_LA$Comp  <- PMagg_LA$Emissions/ (PMagg_LA[which(PMagg_LA$Year==1999),]$Emissions)
+    PMagg_LA$Location = "Los Angeles County" ## add location variable
+    PMagg_LA$Comp  <- PMagg_LA$Emissions/ 
+        (PMagg_LA[which(PMagg_LA$Year==1999),]$Emissions) ## add normalised comparison
+    
+    ## combine data sets
     PMagg <- rbind(PMagg_LA, PMagg_BC)
+    
     ## plot the data!
     plot6 <- qplot(Year,Comp, data=PMagg, color=Location, geom="line", 
                    ylab="Normalised Comparision from 1999", 
